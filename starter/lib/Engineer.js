679af1694e006b8c600cf4bed93efc1bab7eb145
Michael Ergette <michael_e96@hotmail.co.uk>
2024-02-21 23:37:47 +0000

    removed comments

--- starter/lib/Engineer.js
+++ starter/lib/Engineer.js
@@ -1,12 +1,4 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
-// const Engineer = class extends Employee {
-//     constructor(){
-//         super(id, name, email, officeNumber);
-// //name error?
-
-//         this.github = github
-//     }
-// }
 const Employee = require("./Employee");
 
 class Engineer extends Employee {
